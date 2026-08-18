986552e9cc561f348e8706f566f903b512003c24
Nic Westvold <nic.westvold@grafana.com>
2026-08-18 11:33:37 -0400

    fix(rspack): write the swagger build to public/build-swagger

--- eslint.config.js
+++ eslint.config.js
@@ -135,7 +135,6 @@ module.exports = [
       'packages/grafana-ui/src/graveyard', // deprecated UI components slated for removal
       'public/build-rspack', // build output (rspack)
       'public/build-swagger', // swagger build output
-      'public/build-swagger-rspack', // swagger build output (rspack)
       'apps/plugins/plugin/src/generated/meta/v0alpha1',
       'apps/plugins/plugin/src/generated/plugin/v0alpha1',
       'packages/get-document/index.js',
